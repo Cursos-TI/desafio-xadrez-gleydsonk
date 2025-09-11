ebb695ad352b2a4268ed7d2217e09afbcbb7054a
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-11 21:10:36 +0000

    nivelaventureiro

---
+++ Nilvel_Aventureiro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int movimentoCompleto = 1;
+
+    while (movimentoCompleto--)
+        {
+            for (int i = 0; i < 2; i++) {
+                printf("Cima\n");
+            }
+            printf("Direita");
+        }
+        
+
+    return 0;
+}
